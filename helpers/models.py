7e84b1fe3be59fc1aaebf1a1f3a805b01536ca51
crycetruly <crycetruly@gmail.com>
2021-04-18 10:39:16 +0300

    update tracking model

--- helpers/models.py
+++ helpers/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class TrackingModel(models.Model):
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
